00148dc48b487fbbfdebc5394a9b2953da329b12
ledesmx <alberto.ledesma.dev@gmail.com>
2026-06-24 17:56:19 -0600

    save post in memory with post /posts endpoint

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -27,5 +28,9 @@ def get_posts():
 
 @app.post("/posts")
 def create_post(new_post: Post):
+    post_dict = new_post.model_dump()
+    # Usually tha DB do this, but for now I'm going to hardcode it
+    post_dict["id"] = randrange(0,1000)
+    posts.append(post_dict)
     print(new_post)
-    return {"newpost": f"title: {new_post.title}  -  content: {new_post.content}"}
+    return {"data": post_dict}
