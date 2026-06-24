f4ee1854f20f7310724a8b059d164f2861a24bb3
ledesmx <alberto.ledesma.dev@gmail.com>
2026-06-23 21:32:15 -0600

    get data from endpoint /createpost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -11,5 +12,6 @@ def get_posts():
     return {"data": "Post 1"}
 
 @app.post("/createpost")
-def create_post():
-    return {"message": "Post created successfully"}
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"newpost": f"title: {payload.get("title")}  -  content: {payload.get("content")}"}
