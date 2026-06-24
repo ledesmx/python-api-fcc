40aba67a97ff5a385b87e274a49d9047626c0b28
ledesmx <alberto.ledesma.dev@gmail.com>
2026-06-23 22:10:33 -0600

    add Post schema validation with pydantic

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -12,6 +20,11 @@ def get_posts():
     return {"data": "Post 1"}
 
 @app.post("/createpost")
+def create_post(new_post: Post):
+    print(new_post)
+    return {"newpost": f"title: {new_post.title}  -  content: {new_post.content}"}
+
+@app.post("/createpost2")
 def create_post(payload: dict = Body(...)):
     print(payload)
     return {"newpost": f"title: {payload.get("title")}  -  content: {payload.get("content")}"}
